068de6d38bda867998415b21bd7273250921324a
Mateus Moraes <mtu_h@hotmail.com>
2021-06-15 17:59:27 -0300

    referencia
    https://www.mobilemotion.eu/?p=2442

---
+++ CallBlocker/CallBlocker.Android/PhoneCallDetector.cs
@@ -0,0 +1,18 @@
+﻿using Android.Telephony;
+
+namespace CallBlocker.Droid
+{
+    public class PhoneCallDetector : PhoneStateListener
+    {
+        public override void OnCallStateChanged(CallState state, string incomingNumber)
+        {
+            base.OnCallStateChanged(state, incomingNumber);
+
+            if (state == CallState.Ringing)
+            {
+                var number = incomingNumber;
+                // TODO check number for known contact...
+            }
+        }
+    }
+}
