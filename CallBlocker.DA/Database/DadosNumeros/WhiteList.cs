5bf803026dd517ea1f818220894bb8874d605fb6
Mateus Moraes <mtu_h@hotmail.com>
2021-08-18 22:48:47 -0300

    Adicionado Projeto DataBase

---
+++ CallBlocker.DA/Database/DadosNumeros/WhiteList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CallBlocker.Lib.Interfaces.DadosNumeros;
+using Simple.Sqlite;
+using CallBlocker.DA.Models.DadosNumeros;
+
+namespace CallBlocker.DA.Database.DadosNumeros
+{
+    public class WhiteList : IWhiteList
+    {
+        SqliteDB DB;
+        public WhiteList(SqliteDB DB)
+        {
+            this.DB = DB;
+            DB.CreateTables().Add<WhiteList_Model_DA>().Commit();
+        }
+        public void AddNumber_WhiteList(string number, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CallBlocker.DA/Models/DadosNumeros/BlackList_Model_DA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallBlocker.DA.Models.DadosNumeros
+{
+    public class BlackList_Model_DA
+    {
+        public int ID { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+    }
+}
